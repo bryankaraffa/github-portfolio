d59b98de1c8fcd149988e8e14b66f5298729ec11
Bryan Karaffa <bryankaraffa@gmail.com>
2016-02-21 17:11:07 -0800

    Initial working revision.  Lots more polishing to do.

---
+++ index.php
@@ -0,0 +1,106 @@
+<?php
+require_once('./config.php');
+error_reporting(0);
+
+require_once('./includes/github-api-1.4.3/src/github-api.php');
+use Milo\Github;
+
+require_once('./includes/php-markdown/Michelf/Markdown.inc.php');
+use \Michelf\Markdown;
+
+function url_exists($url) {
+    $file = $url;
+    $file_headers = @get_headers($file);
+    $exists = true;
+    foreach ($file_headers as $file_header) {
+        if($file_header == 'HTTP/1.1 404 Not Found') {
+            $exists = false;
+        }
+    }
+    return $exists;
+}
+
+///// Begin Logic
+
+$api = new Github\Api;
+$api->setToken(new Milo\Github\OAuth\Token($github_Api_Key));
+$events = $api->get('/users/bryankaraffa/events');
+$events = $api->decode($events);
+
+$repos = [];
+$readme = [];
+foreach ($events as $event) {
+    $repo = $api->get('/repos/'.$event->repo->name);
+    $repo = $api->decode($repo);
+
+    if (!isset($repos[$repo->id])) { 
+        $repos[$repo->id]=$repo;
+        $r=file_get_contents("https://rawgit.com/".$event->repo->name."/master/README.md");
+        if ($r != '') {
+            $readme[$repo->id]=$r;
+        }
+        else {
+            $readme[$repo->id]='';    
+        }                 
+    }
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <!--Import Google Icon Font-->
+    <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+
+
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+<body>
+
+    <div class="container">
+        <!-- Page Content goes here -->
+<?php 
+    foreach ($repos as $repo) {
+        $readme_content = Markdown::defaultTransform($readme[$repo->id]);
+        if (url_exists('https://rawgit.com/'.$repo->full_name.'/master/screenshot.png')) {
+            $card_image= '
+                <div class="card-image waves-effect waves-block waves-light">
+                  <img class="activator" src="https://rawgit.com/'.$repo->full_name.'/master/screenshot.png">
+                </div>
+            ';
+        }
+        else {
+            $card_image='';
+        }
+        echo ('
+            <div class="card hoverable small">
+                '.$card_image.'
+                <div class="card-content">
+                    <span class="card-title activator grey-text text-darken-4 truncate">'.$repo->full_name.'<i class="material-icons right">more_vert</i></span>
+                    <div class="col s6"><div class="chip left-align">'.$repo->language.'</div></div>
+                    <div class="col s6"><p class="right-align"><a href="'.$repo->html_url.'">Last Updated: '.$repo->updated_at.'</a></p></div>
+                </div>
+                <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">'.$repo->full_name.'<i class="material-icons right">close</i></span>
+                <p>'.$readme_content.'</p>
+                </div>
+            </div>
+            ');
+    }
+?>
+    </div>
+  <!-- Compiled and minified JavaScript -->
+  <script type="text/javascript" src="//code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+
+</body>
+</html>
+<?php
+var_dump($repos);
+?>      
