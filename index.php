f6d736e6ee280b5f4e0f2b1f37c823119942d7be
Bryan Karaffa <bryankaraffa@gmail.com>
2019-11-29 12:40:12 -0800

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ error_reporting(0);
 if ($_GET['debug'] == "true") {
     error_reporting(E_ALL);
     var_dump($github_Api_Key);
+    var_dump($_ENV['github-api-key']);
 }
 
 require_once('./includes/github-api-1.4.3/src/github-api.php');
